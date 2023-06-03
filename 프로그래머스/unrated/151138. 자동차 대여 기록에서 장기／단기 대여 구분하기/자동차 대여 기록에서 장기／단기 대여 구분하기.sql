61cdd648a3cb588f3d450d35b568b396ee03a98a
s2cocos2 <120706046+s2cocos2@users.noreply.github.com>
2023-06-03 21:28:37 +0900

    [unrated] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT HISTORY_ID, CAR_ID, DATE_FORMAT(START_DATE, '%Y-%m-%d') as START_DATE, DATE_FORMAT(END_DATE, '%Y-%m-%d') as END_DATE,
+       (case when DATEDIFF(END_DATE, START_DATE	)+1 >= 30 then '장기 대여'
+       else '단기 대여' end) as RENT_TYPE
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+where START_DATE between '2022-09-01' and '2022-09-30'
+order by HISTORY_ID desc
