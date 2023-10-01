6c997e2ebfa08dc6013cb50453fc36ea8dec5b39
s2cocos2 <120706046+s2cocos2@users.noreply.github.com>
2023-10-01 20:54:15 +0900

    [level 0] Title: 가위 바위 보, Time: 2.11 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        for(int i=0; i<rsp.length(); i++){
+            char a = rsp.charAt(i);
+            if(a=='0'){
+                answer += '5';
+            } else if (a == '2'){
+                answer += '0';
+            } else {
+                answer += '2';
+            }
+        }
+        
+        return answer;
+    }
+}
