beab0c56a3c553f3b2bf647885cceaea953c0108
s2cocos2 <120706046+s2cocos2@users.noreply.github.com>
2023-10-10 23:22:04 +0900

    [level unrated] Title: 외계행성의 나이, Time: 17.67 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        String ageString = String.valueOf(age);
+
+        for(int i=0;i<ageString.length();i++){
+            answer += (char)(ageString.charAt(i)+49);
+        }
+
+        return answer;
+    }
+}
