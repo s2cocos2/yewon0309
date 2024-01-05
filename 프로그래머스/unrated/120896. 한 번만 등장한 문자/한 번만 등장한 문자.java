00651e757ce7a6b95146e63116f52281fd162331
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-01-05 11:10:58 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 2.21 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+        Arrays.sort(str);
+
+        for(int i=0;i<s.length();i++){
+            int count = 0;
+            for(int j=0;j<s.length();j++){
+                if(str[i].equals(str[j])){
+                    count++;
+                }
+            }
+            if(count == 1){
+                answer += str[i];
+            }
+        }
+
+        return answer;
+    }
+}
