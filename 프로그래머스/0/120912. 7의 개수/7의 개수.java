ac6a25e523e9f16243b6e0de7c563d7e5a4e6803
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-01-22 15:41:02 +0900

    [level 0] Title: 7의 개수, Time: 5.88 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String[] strArr = new String[array.length];
+        for(int i=0;i<array.length;i++){
+            strArr[i] = array[i] + "";
+        }
+        
+        String str = "";
+        for(int i=0;i<strArr.length;i++){
+            str += strArr[i];
+        }
+
+        int count = 0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == '7'){
+                count++;
+            }
+        }
+
+        answer = count;
+        return answer;
+    }
+}
