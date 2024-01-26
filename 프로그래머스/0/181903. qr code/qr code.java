a1dbb1c798583ab89421d9e791d64f96c380cfd2
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-01-26 16:00:57 +0900

    [level 0] Title: qr code, Time: 12.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,8 +1,10 @@
 class Solution {
     public String solution(int q, int r, String code) {
         String answer = "";
-        for(int i=0;i*q+r<code.length();i++){
-            answer += code.charAt(i*q+r);
+        for(int i=0;i<code.length();i++){
+            if(i % q == r){
+                answer += code.charAt(i);
+            }
         }
         return answer;
     }
