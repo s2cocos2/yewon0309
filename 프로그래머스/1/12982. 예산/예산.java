2ce204c7c5c82a9533f6765b06e1e35f2c95eb1a
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-01-29 22:16:13 +0900

    [level 1] Title: 예산, Time: 0.38 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -7,12 +7,11 @@ class Solution {
         Arrays.sort(d);
         int sum = 0;
         for(int i=0;i<d.length;i++){
-            if(sum + d[i] <= budget){
-                sum += d[i];
-                answer++;
-            } else {
+            budget -= d[i];
+            if(budget < 0){
                 break;
             }
+            answer++;
         }
         return answer;
     }
