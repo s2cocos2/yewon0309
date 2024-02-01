573fc7c01cf4dfcea9a35abc4b9176ea078e2f7d
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-02-01 11:35:15 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+
+        for(int i=0;i<my_string.length();i++){
+            char c = my_string.charAt(i);
+            if('A' <= c && c <= 'Z'){
+                answer[c-'A']++;
+            } else {
+                answer[c - 'a' + 26]++;
+            }
+
+        }
+        return answer;
+    }
+}
