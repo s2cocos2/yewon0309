436fb88fd0763de433cff2a49a7168310442a145
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-02-06 17:17:02 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        int answer = A + B;
+        
+        System.out.println(answer);
+    }
+}
