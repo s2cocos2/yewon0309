fe4b3b2a97e7f2a2141375c4ce2b4194a305e0b8
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-02-21 18:28:45 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int answer = A * B;
+        
+        System.out.println(answer);
+    }
+}
