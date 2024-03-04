95b426d2bc37890b8d6f907d2bed43f218eb73f5
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-03-04 12:34:18 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,11 +1,7 @@
 class Solution {
     public int solution(int M, int N) {
-        int answer = 0;        
-        if(M == 1 && N == 1){
-            answer = 0;
-        } else {
-            answer = (M-1) + (N-1)*M;
-        }
+        int answer = 0;
+        answer = (M-1) + (N-1)*M;
         return answer;
     }
 }
