27ed22ca56ad704d7129c44a5fec854bfda92b4e
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-03-13 19:43:07 +0900

    [level 0] Title: 등수 매기기, Time: 0.26 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -1,20 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[][] score) {
         int[] answer = new int[score.length];
 
-        int[] avg = new int[score.length];
-        for(int i=0;i< score.length;i++){
-            avg[i] = score[i][0] + score[i][1];
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<score.length;i++){
+            list.add(score[i][0] + score[i][1]);
         }
 
-        for(int i=0;i<avg.length;i++){
-            int rank = 1;
-            for(int j=0;j<avg.length;j++){
-                if(avg[i] < avg[j]){
-                    rank++;
-                }
-            }
-            answer[i] = rank;
+        list.sort(Collections.reverseOrder());
+
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.indexOf(score[i][0] + score[i][1])+1;
         }
 
         return answer;
