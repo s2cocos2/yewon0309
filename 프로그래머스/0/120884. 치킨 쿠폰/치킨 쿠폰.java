53e09f32662be1d33523a7f41bac4a5b56824b83
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-03-15 11:04:47 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+
+        int service = 0;
+        while(chicken >= 10){
+            service = chicken / 10;
+            answer += service;
+            chicken = service + (chicken % 10);
+        }
+        return answer;
+    }
+}
