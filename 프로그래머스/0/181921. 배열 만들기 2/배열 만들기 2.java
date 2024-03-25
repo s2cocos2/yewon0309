901c96e666edd25f5e9f848c9cbab59e07b9383c
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-03-25 13:55:20 +0900

    [level 0] Title: 배열 만들기 2, Time: 10.14 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=l;i<=r;i++){
+            String iStr = String.valueOf(i);
+            boolean b = true;
+            for(int j=0;j<iStr.length();j++){
+                char c = iStr.charAt(j);
+                if(c != '0' && c != '5'){
+                    b = false;
+                    break;
+                }
+            }
+            if(b){
+                list.add(i);
+            }
+        }
+
+        Collections.sort(list);
+        int[] answer;
+        if(list.isEmpty()){
+            answer = new int[]{-1};
+        } else {
+            answer = list.stream().mapToInt(Integer::intValue).toArray();
+        }
+        return answer;
+    }
+}
