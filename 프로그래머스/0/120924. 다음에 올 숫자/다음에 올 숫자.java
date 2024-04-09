95aa12d6a4b740e8a90005068bbc50b17ca213cf
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-04-09 14:25:36 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         if((common[1] - common[0]) == (common[2] - common[1])){
             answer = common[common.length-1] + (common[1] - common[0]);
-        } else if((common[1] / common[0]) == (common[2] / common[1])) {
+        } else {
             answer = common[common.length-1] * (common[1] / common[0]);
         }
         return answer;
