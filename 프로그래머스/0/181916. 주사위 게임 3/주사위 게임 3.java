db0800fae59a795d4e3ed7c3c274d90e3028b7ce
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-04-23 17:09:21 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int answer = 0;
+        
+        int[] arr = new int[7];
+        arr[a]++;
+        arr[b]++;
+        arr[c]++;
+        arr[d]++;
+
+        int one = 0, two = 0, three = 0, four = 0, max = 0;
+        for(int i=1;i<=6;i++){
+            if(arr[i] == 1) one = i;
+            if(arr[i] == 2) two = i;
+            if(arr[i] == 3) three = i;
+            if(arr[i] == 4) four = i;
+            max = Math.max(max, arr[i]);
+        }
+
+
+        if(four > 0){
+            answer = 1111 * four;
+        } else if(three > 0){
+            answer = (10 * three + one) * (10 * three + one);
+        } else if(two > 0){
+            for(int i=1;i<=6;i++){
+                if(one + two + three + four == two){
+                    int two2 = 0;
+                    for(int j=1;j<=6;j++){
+                        if(arr[j] == 2 && two != j){
+                            two2 = j;
+                        }
+                    }
+                    answer = (two + two2) * Math.abs(two - two2);
+                } else {
+                    int num1 = 0, num2 = 0;
+                    for(int j=1;j<=6;j++){
+                        if(arr[j] == 1){
+                            num1 = j;
+                            break;
+                        }
+                    }
+                    for(int j=1;j<=6;j++){
+                        if(arr[j] == 1 && j != num1){
+                            num2 = j;
+                            break;
+                        }
+                    }
+
+                    answer = num1 * num2;
+                }
+
+            }
+       } else {
+            List<Integer> list = new ArrayList<>();
+            list.add(a);
+            list.add(b);
+            list.add(c);
+            list.add(d);
+            int min = Collections.min(list);
+
+            answer = min;
+        }
+
+        return answer;
+    }
+}
