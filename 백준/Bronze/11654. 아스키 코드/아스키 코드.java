c616383878251fc3aa46e33119ce93702faad3f1
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-05-01 10:50:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String t = sc.nextLine();
+        
+        char c = t.charAt(0);
+        System.out.println((int)c);
+
+    }
+}
