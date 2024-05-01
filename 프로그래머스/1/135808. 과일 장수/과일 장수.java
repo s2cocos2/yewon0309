dedf2baadfb1801ab28dc47e54def706189d3818
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-05-01 17:30:23 +0900

    [level 1] Title: 과일 장수, Time: 0.50 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,25 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-            int boxCnt = score.length / m;
-            List<Integer> list = new ArrayList<>();
-            for(int i=0;i<score.length;i++){
-                list.add(score[i]);
-            }
-
-            Collections.sort(list, Collections.reverseOrder());
-
-            List<Integer> list2 = new ArrayList<>();
-            for(int i=0;i<boxCnt;i++){
-                List<Integer> temp = list.subList(i*m, i*m + m);
-                int min = Collections.min(temp);
-                list2.add(min);
-            }
-
-
-            for(int i=0;i<list2.size();i++){
-                answer += list2.get(i) * m;
-            }
+    
+        Arrays.sort(score);
+        for(int i=score.length;i>=m;i-=m){
+            answer += score[i-m] * m;
+        }
+        
         return answer;
     }
 }
