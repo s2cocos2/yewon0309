3d12b8dace90f8408039f5a30afe290149a0defb
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-05-07 12:08:45 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,26 +1,25 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String S = scanner.nextLine();
-        
-        int[] alpha = new int[26]; 
-        
-        for (int i = 0; i < 26; i++) {
-            alpha[i] = -1;
-        }
-        
-        for (int i = 0; i < S.length(); i++) {
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.nextLine();
+
+        int[] answer = new int[26];
+        Arrays.fill(answer, -1);
+
+        for(int i=0;i<S.length();i++){
             int index = S.charAt(i) - 'a';
-            
-            if (alpha[index] == -1) {
-                alpha[index] = i;
+            if(answer[index] == -1){
+                answer[index] = i;
             }
+
         }
         
-        for (int i = 0; i < 26; i++) {
-            System.out.print(alpha[i] + " ");
+        for(int i=0;i<answer.length;i++){
+            System.out.println(answer[i]);
         }
+
     }
 }
