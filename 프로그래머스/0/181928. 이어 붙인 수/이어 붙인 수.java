1f6ed315262b6affac1cd051dc952c1361fc87d6
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-05-11 11:57:50 +0900

    [level 0] Title: 이어 붙인 수, Time: 10.71 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -2,16 +2,16 @@ class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
         String odd = "";
-        String even = "";
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] % 2 == 0) {
-                even += Integer.toString(num_list[i]);
-            } else {
-                odd += Integer.toString(num_list[i]);
+            String even = "";
+            for(int i=0;i<num_list.length;i++){
+                if(num_list[i] % 2 == 0){
+                    odd += num_list[i] + "";
+                } else {
+                    even += num_list[i] + "";
+                }
             }
-        }
-        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+
+            answer = Integer.parseInt(odd) + Integer.parseInt(even);
         return answer;
     }
 }
-//
