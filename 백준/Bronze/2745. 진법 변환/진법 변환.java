027b21c0a0e187bf2e4601f84e5eb88c462aded8
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-06-04 10:24:00 +0900

    [Bronze II] Title: 진법 변환, Time: 184 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String N = sc.next();
+        int B = sc.nextInt();
+
+        int num = Integer.parseInt(N, B);
+
+        System.out.println(num);
+
+
+        }
+}
