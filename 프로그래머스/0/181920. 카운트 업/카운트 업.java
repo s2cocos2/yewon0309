092ac4469b7d58cebdd6f13624e8935e9dffab03
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-06-07 12:10:55 +0900

    [level 0] Title: 카운트 업, Time: 2.84 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,14 +1,13 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int start, int end) {
-        List<Integer> answer = new ArrayList<>();
-        
-        for (int i = start; i <= end; i++) {
-            answer.add(i);
+    public int[] solution(int start_num, int end_num) {
+        List<Integer> list = new ArrayList<>();
+        for(int i=start_num;i<=end_num;i++){
+            list.add(i);
         }
-        
-        return answer.stream().mapToInt(i->i).toArray();
+
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 }
-//
