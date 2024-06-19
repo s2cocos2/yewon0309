1774f353ce236365456f971cb63eae640f990a59
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-06-19 20:16:14 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        if(my_string.length() >= is_suffix.length()){
+            String newMyString = my_string.substring(my_string.length()-is_suffix.length());
+            if(newMyString.equals(is_suffix)){
+                answer = 1;
+            } else {
+                answer = 0;
+            }
+        } else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
