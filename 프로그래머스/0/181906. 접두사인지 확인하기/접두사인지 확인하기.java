bc1e69be379d87db18ae608d9c9f5b9502cae58b
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-06-22 21:35:42 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,11 +1,8 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
         int answer = 0;
-        if(my_string.startsWith(is_prefix)){
-            answer = 1;
-        } else {
-            answer = 0;
-        }
+        answer = my_string.startsWith(is_prefix) ? 1 : 0;
+ 
         return answer;
     }
 }
