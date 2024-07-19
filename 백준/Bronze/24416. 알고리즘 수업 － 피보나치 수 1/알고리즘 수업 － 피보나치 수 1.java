2b4d5b8153ea81c4f7c8b11bca8d4e525cef170d
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-07-19 10:39:18 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 596 ms, Memory: 18528 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Main {
+
+    static int countFib = 0;
+    static int countFibonacci = 0;
+    static int[] f;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        fib(N);
+        fibonacci(N);
+
+        System.out.println(countFib + " " + countFibonacci);
+    }
+
+    public static int fib(int n){
+        if (n == 1 || n == 2){
+            countFib++;
+            return 1;
+        } else {
+            return (fib(n - 1) + fib(n - 2));
+        }
+    }
+
+    public static int fibonacci(int n){
+        f = new int[n+1];
+        f[0] = f[1] = 1;
+        for(int i=3;i<=n;i++){
+            f[i] = f[i - 1] + f[i - 2];
+            countFibonacci++;
+        }
+        return f[n-1];
+    }
+}
