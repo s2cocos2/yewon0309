64282f5d7b6b7eabeb0ecf766f7ce69fd97d29a7
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-10-03 23:13:26 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 203.59 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String answer = "";
         String a = sc.next();
         String b = sc.next();
-        System.out.print(a+b);
+        answer = a + b;
+        System.out.print(answer);
     }
 }
