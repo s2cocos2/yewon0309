1909e95ad06a9c6c64359324ca1fe23b358c0eaf
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-10-04 22:58:20 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 2.88 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,16 +1,24 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
-        String answer = "";
         StringBuilder sb = new StringBuilder(my_string);
 
-        for(int i=0;i<queries.length;i++){
-            StringBuilder sb2 = new StringBuilder();
-            sb2.append(sb.substring(queries[i][0],queries[i][1]+1));
-            sb2.reverse();
-            sb.replace(queries[i][0],queries[i][1]+1,sb2.toString());
+        for (int[] query : queries) {
+            int start = query[0];
+            int end = query[1];
+
+            String reversedPart = new StringBuilder(sb.substring(start, end + 1)).reverse().toString();
+            sb.replace(start, end + 1, reversedPart);
         }
 
-        answer = sb.toString();
-        return answer;
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String my_string = "rermgorpsam";
+        int[][] queries = {{2, 3}, {0, 7}, {5, 9}, {6, 10}};
+
+        String result = sol.solution(my_string, queries);
+        System.out.println(result);
     }
 }
