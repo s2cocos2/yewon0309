e40320cb864bc75757387efbc62ceafa4bd69a9c
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-10-06 23:13:48 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.29 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -1,20 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
-    public int[] solution(int n) {
-        List<Integer> listAnswer = new ArrayList<>();
-        while (n!=1){
-            listAnswer.add(n);
-            if(n%2==0){
-                n = n/2;
-            } else {
-                n = 3 * n +1;
+public class Solution {
+    public List<Integer> solution(int n) {
+        List<Integer> collatzSequence = new ArrayList<>();
+
+        while (n != 1) {
+            collatzSequence.add(n);
+            if (n % 2 == 0) {
+                n /= 2;
+            } 
+            else {
+                n = 3 * n + 1;
             }
         }
-        listAnswer.add(1);
 
-        int[] answer = listAnswer.stream().mapToInt(Integer::intValue).toArray();
-        return answer;
+        // 마지막으로 1을 추가
+        collatzSequence.add(1);
+
+        return collatzSequence;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        int n = 10;
+        List<Integer> result = sol.solution(n);
+
+        System.out.println(result);
     }
 }
