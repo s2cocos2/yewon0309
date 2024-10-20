a1d890f529596ade7f3ffc9f02b54c2b207e3f12
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-10-20 22:47:30 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,15 +1,16 @@
 class Solution {
     public int solution(int[] number) {
-        int answer = 0;
-        for(int i=0;i<number.length;i++){
-            for(int j=i+1;j<number.length;j++){
-                for(int k=j+1;k<number.length;k++){
-                    if(number[i] + number[j] + number[k] == 0){
-                        answer++;
+        int count = 0;
+        for (int i = 0; i < number.length - 2; i++) {
+            for (int j = i + 1; j < number.length - 1; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        count++;
                     }
                 }
             }
         }
-        return answer;
+
+        return count;
     }
 }
