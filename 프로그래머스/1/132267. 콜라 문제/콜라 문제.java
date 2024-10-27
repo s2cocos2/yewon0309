6feaa50e3023691c43074cd7f2c1b7095fa4b4ef
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-10-27 23:03:00 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,12 +1,13 @@
-class Solution {
+public class Solution {
     public int solution(int a, int b, int n) {
-        int answer = 0;
-        while (n >= a){
-            int num1 = 0;
-            num1 = n / a;
-            answer += num1 * b;
-            n = num1 * b + (n % a);
+        int totalCola = 0;
+        while (n >= a) {
+            int newCola = (n / a) * b;
+            totalCola += newCola;
+            
+            n = (n % a) + newCola;
         }
-        return answer;
+
+        return totalCola;
     }
 }
