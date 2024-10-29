3212929531df663f5fe7daef8907c100f3b90d8a
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-10-29 23:52:22 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.13 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,8 +1,10 @@
 public class Solution {
     public String solution(String phone_number) {
         int length = phone_number.length();
-        String stars = "*".repeat(length - 4);
-        String lastFourDigits = phone_number.substring(length - 4);
-        return stars + lastFourDigits;
+        
+        String hiddenPart = "*".repeat(length - 4);
+        
+        String visiblePart = phone_number.substring(length - 4);
+        return hiddenPart + visiblePart;
     }
 }
