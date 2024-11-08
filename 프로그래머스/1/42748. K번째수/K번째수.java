23ddb4339d1f08f2f9215a92903d54bb6e35d88f
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-11-08 22:37:08 +0900

    [level 1] Title: K번째수, Time: 0.96 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,18 +3,19 @@ import java.util.Arrays;
 public class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] result = new int[commands.length];
-        
-        for (int idx = 0; idx < commands.length; idx++) {
-            int i = commands[idx][0];
-            int j = commands[idx][1];
-            int k = commands[idx][2];
 
-            int[] slicedArray = Arrays.copyOfRange(array, i - 1, j);
-            Arrays.sort(slicedArray);
+        for (int c = 0; c < commands.length; c++) {
+            int i = commands[c][0];
+            int j = commands[c][1];
+            int k = commands[c][2];
             
-            result[idx] = slicedArray[k - 1];
+            int[] subArray = Arrays.copyOfRange(array, i - 1, j);
+
+            Arrays.sort(subArray);
+            
+            result[c] = subArray[k - 1];
         }
-        
+
         return result;
     }
 }
