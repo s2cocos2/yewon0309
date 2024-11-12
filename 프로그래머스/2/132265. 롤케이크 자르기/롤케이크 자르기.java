cef1fe1eb708ee20154ab4d5962a75f6fc3b79aa
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-11-12 23:12:06 +0900

    [level 2] Title: 롤케이크 자르기, Time: 258.19 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,35 +1,32 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
 
-class Solution {
+public class Solution {
     public int solution(int[] topping) {
-        int answer = 0;
-        
-        Set<Integer> leftSet = new HashSet<>();
-        Set<Integer> rightSet = new HashSet<>();
-        
-        int[] rightCount = new int[10001];
-        for (int t : topping) {
-            rightSet.add(t);
-            rightCount[t]++;
+        int count = 0;
+        HashSet<Integer> chulsooToppings = new HashSet<>();
+        HashMap<Integer, Integer> siblingToppings = new HashMap<>();
+
+        for (int top : topping) {
+            siblingToppings.put(top, siblingToppings.getOrDefault(top, 0) + 1);
         }
-        
-        int[] leftCount = new int[10001];
-        
-        for (int i = 0; i < topping.length - 1; i++) {
-            int currentTopping = topping[i];
-            leftSet.add(currentTopping);
-            leftCount[currentTopping]++;
-            
-            rightCount[currentTopping]--;
-            if (rightCount[currentTopping] == 0) {
-                rightSet.remove(currentTopping);
+
+        for (int i = 0; i < topping.length; i++) {
+            int currentTop = topping[i];
+
+            chulsooToppings.add(currentTop);
+
+            siblingToppings.put(currentTop, siblingToppings.get(currentTop) - 1);
+            if (siblingToppings.get(currentTop) == 0) {
+                siblingToppings.remove(currentTop);
             }
-            
-            if (leftSet.size() == rightSet.size()) {
-                answer++;
+
+            if (chulsooToppings.size() == siblingToppings.size()) {
+                count++;
             }
         }
-        return answer;
+        
+        return count;
     }
 }
 //
