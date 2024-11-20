99cd137ee85303b00591a8ba8f1c1bfe06ad7b4e
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-11-20 23:34:56 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 21.26 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,28 +1,14 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        List<Integer> list = new ArrayList<>();
-        for(int i=1;i<food.length;i++){
-            for(int j=1;j<=food[i]/2;j++){
-                list.add(i);
+        StringBuilder left = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+            for (int j = 0; j < count; j++) {
+                left.append(i);
             }
         }
-        List<Integer> list2 = new ArrayList<>(list);
-        Collections.sort(list2, Collections.reverseOrder());
 
-        list.add(0);
-        list.addAll(list2);
-
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<list.size();i++){
-            sb.append(list.get(i));
-        }
-
-        answer = sb.toString();
-        return answer;
+        String result = left.toString() + "0" + left.reverse().toString();
+        return result;
     }
 }
