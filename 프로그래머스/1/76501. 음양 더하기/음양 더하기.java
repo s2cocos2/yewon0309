e49eccbbe2029c8cce56d082b455fe6019b067ab
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-11-29 23:51:53 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int sum = 0;
-
+        
         for (int i = 0; i < absolutes.length; i++) {
-            if (signs[i]) {
-                sum += absolutes[i];
-            } else {
-                sum -= absolutes[i];
-            }
+            sum += signs[i] ? absolutes[i] : -absolutes[i];
         }
-
+        
         return sum;
     }
 }
