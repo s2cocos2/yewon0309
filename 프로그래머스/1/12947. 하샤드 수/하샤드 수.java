45835defc51530bb69a83981adc313a2054ed9c9
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-11-30 23:25:16 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,13 +1,12 @@
-public class Solution {
+class Solution {
     public boolean solution(int x) {
-        int sumOfDigits = 0;
-        int temp = x;
-
+        int sumOfDigits = 0; 
+        int temp = x; 
         while (temp > 0) {
             sumOfDigits += temp % 10;
             temp /= 10;
         }
-
+        
         return x % sumOfDigits == 0;
     }
 }
