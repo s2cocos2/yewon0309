c41cfe6a39716b2f79bc0fa5fde089b3957c8262
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2024-12-01 14:54:57 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 7.90 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,11 +1,14 @@
 public class Solution {
     public String solution(String[] seoul) {
         int index = -1;
-        for (int i = 0; i < seoul.length; i++) {
-            if (seoul[i].equals("Kim")) {
+        int i = 0;
+        
+        for (String name : seoul) {
+            if (name.equals("Kim")) {
                 index = i;
                 break;
             }
+            i++;
         }
         
         return "김서방은 " + index + "에 있다";
