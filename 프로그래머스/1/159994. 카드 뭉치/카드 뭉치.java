7c65c2c9237d8829fda33d5abf1bd714dd19681d
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-03 23:16:24 +0900

    [level 1] Title: 카드 뭉치, Time: 0.14 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,19 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-        int index1 = 0;
-        int index2 = 0;
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
 
-        answer = "Yes";
-        for(int i=0;i<goal.length;i++){
-            if(index1 < cards1.length && goal[i].equals(cards1[index1])){
-                index1++;
-            } else if(index2 < cards2.length && goal[i].equals(cards2[index2])){
-                index2++;
+        for (String card : cards1) {
+            queue1.add(card);
+        }
+        for (String card : cards2) {
+            queue2.add(card);
+        }
+
+        for (String word : goal) {
+            if (!queue1.isEmpty() && queue1.peek().equals(word)) {
+                queue1.poll();
+            } else if (!queue2.isEmpty() && queue2.peek().equals(word)) {
+                queue2.poll();
             } else {
-                answer = "No";
+                return "No";
             }
         }
-        return answer;
+
+        return "Yes";
     }
 }
