4b48be2036e770bc2056c63be57b7c91e93239f2
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-05 22:36:46 +0900

    [level 1] Title: 평균 구하기, Time: 3.36 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,9 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public double solution(int[] arr) {
-        int sum = 0;
-        for (int num : arr) {
-            sum += num;
-        }
-        return (double) sum / arr.length;
+        return Arrays.stream(arr).average().orElse(0.0);
     }
 }
