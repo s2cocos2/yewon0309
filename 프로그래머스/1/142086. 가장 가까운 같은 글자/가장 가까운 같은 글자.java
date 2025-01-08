c7d387c1cfb9066b7be1d03d33d972b8d50148e8
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-08 23:32:44 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.09 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,22 +1,24 @@
-import java.util.HashMap;
-
 public class Solution {
     public int[] solution(String s) {
         int[] result = new int[s.length()];
-        HashMap<Character, Integer> lastSeen = new HashMap<>();
-
+        int[] lastSeen = new int[26];
+        for (int i = 0; i < 26; i++) {
+            lastSeen[i] = -1;
+        }
+        
         for (int i = 0; i < s.length(); i++) {
             char currentChar = s.charAt(i);
-
-            if (lastSeen.containsKey(currentChar)) {
-                result[i] = i - lastSeen.get(currentChar);
+            int index = currentChar - 'a';
+            
+            if (lastSeen[index] != -1) {
+                result[i] = i - lastSeen[index];
             } else {
                 result[i] = -1;
             }
-
-            lastSeen.put(currentChar, i);
+            
+            lastSeen[index] = i;
         }
-
+        
         return result;
     }
 }
