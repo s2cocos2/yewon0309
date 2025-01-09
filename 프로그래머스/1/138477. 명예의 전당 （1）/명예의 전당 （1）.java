b4dfd6b74dc5a281a753215b663c6a4e6465038a
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-09 23:17:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 5.23 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,19 +1,19 @@
-import java.util.*;
+import java.util.PriorityQueue;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
+        PriorityQueue<Integer> hallOfFame = new PriorityQueue<>();
 
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        for(int i=0;i<score.length;i++){
-            if(q.size() < k){
-                q.add(score[i]);
-            } else if(q.peek() < score[i]){
-                q.poll();
-                q.add(score[i]);
+        for (int i = 0; i < score.length; i++) {
+            hallOfFame.add(score[i]);
+            if (hallOfFame.size() > k) {
+                hallOfFame.poll();
             }
-            answer[i] = q.peek();
+
+            answer[i] = hallOfFame.peek();
         }
+
         return answer;
     }
 }
