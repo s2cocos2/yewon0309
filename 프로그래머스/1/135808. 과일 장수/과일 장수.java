499f93f4688918bb6331aa153a0773091f4da88b
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-10 00:58:32 +0900

    [level 1] Title: 과일 장수, Time: 257.37 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,12 +1,24 @@
-import java.util.*;
+import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-    
-        Arrays.sort(score);
-        for(int i=score.length;i>=m;i-=m){
-            answer += score[i-m] * m;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        
+        for (int s : score) {
+            pq.add(s);
+        }
+        
+        // 큐에서 m개씩 묶어서 처리
+        while (pq.size() >= m) {
+            int minInBox = Integer.MAX_VALUE;
+
+            for (int i = 0; i < m; i++) {
+                minInBox = Math.min(minInBox, pq.poll());
+            }
+            
+            answer += minInBox * m;
         }
         
         return answer;
