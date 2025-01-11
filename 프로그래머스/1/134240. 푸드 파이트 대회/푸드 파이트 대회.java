d3dd094893bf6242c1fa77eb06f779ad87f2bb56
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-11 23:27:47 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.36 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder left = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        
         for (int i = 1; i < food.length; i++) {
             int count = food[i] / 2;
-            for (int j = 0; j < count; j++) {
-                left.append(i);
+            if (count > 0) {
+                result.append(String.valueOf(i).repeat(count));
             }
         }
 
-        String result = left.toString() + "0" + left.reverse().toString();
-        return result;
+        return result.toString() + "0" + result.reverse().toString();
     }
 }
