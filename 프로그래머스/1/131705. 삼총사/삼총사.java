94be229da48cc850b4731c54179e92cb9051c648
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-13 23:16:54 +0900

    [level 1] Title: 삼총사, Time: 2.09 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,14 +1,17 @@
 class Solution {
     public int solution(int[] number) {
+        return findTriplets(number, 0, 0, 0);
+    }
+
+    private int findTriplets(int[] number, int start, int depth, int sum) {
+        if (depth == 3) {
+            return sum == 0 ? 1 : 0;
+        }
+
         int count = 0;
-        for (int i = 0; i < number.length - 2; i++) {
-            for (int j = i + 1; j < number.length - 1; j++) {
-                for (int k = j + 1; k < number.length; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        count++;
-                    }
-                }
-            }
+
+        for (int i = start; i < number.length; i++) {
+            count += findTriplets(number, i + 1, depth + 1, sum + number[i]);
         }
 
         return count;
