27981161ebe28ee3f1c92e2dda3ae36193562942
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-17 17:03:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,22 +1,10 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int solution(String s) {
-        Map<String, String> numberWords = new HashMap<>();
-        numberWords.put("zero", "0");
-        numberWords.put("one", "1");
-        numberWords.put("two", "2");
-        numberWords.put("three", "3");
-        numberWords.put("four", "4");
-        numberWords.put("five", "5");
-        numberWords.put("six", "6");
-        numberWords.put("seven", "7");
-        numberWords.put("eight", "8");
-        numberWords.put("nine", "9");
+        String[] numberWords = {"zero", "one", "two", "three", "four", 
+                                 "five", "six", "seven", "eight", "nine"};
 
-        for (Map.Entry<String, String> entry : numberWords.entrySet()) {
-            s = s.replaceAll(entry.getKey(), entry.getValue());
+        for (int i = 0; i < numberWords.length; i++) {
+            s = s.replace(numberWords[i], String.valueOf(i));
         }
 
         return Integer.parseInt(s);
