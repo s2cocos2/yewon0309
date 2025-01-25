42d62264983391e0532fda0be653f94b5c27e6b0
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-25 23:27:38 +0900

    [level 1] Title: 모의고사, Time: 9.80 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,35 +1,27 @@
 import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[][] patterns = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
 
-        int[] score = {0, 0, 0};
-
-        for(int i=0;i<answers.length;i++){
-            if(arr1[i % arr1.length] == answers[i]){
-                score[0]++;
-            }
-            if(arr2[i % arr2.length] == answers[i]){
-                score[1]++;
-            }
-            if(arr3[i % arr3.length] == answers[i]){
-                score[2]++;
-            }
+        int[] scores = new int[3];
+        for (int i = 0; i < patterns.length; i++) {
+            int[] pattern = patterns[i];
+            scores[i] = (int) IntStream.range(0, answers.length)
+                                       .filter(j -> answers[j] == pattern[j % pattern.length])
+                                       .count();
         }
 
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
-        List<Integer> list = new ArrayList<>();
-        for(int i=0;i< score.length;i++){
-            if(score[i] == max){
-                list.add(i + 1);
-            }
-        }
+        int maxScore = Arrays.stream(scores).max().orElse(0);
 
-
-        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
-        return answer;
+        return IntStream.range(0, scores.length)
+                        .filter(i -> scores[i] == maxScore)
+                        .map(i -> i + 1)
+                        .toArray();
     }
 }
