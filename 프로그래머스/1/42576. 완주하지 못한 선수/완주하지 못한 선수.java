aa8f14d1e6ab5843d379c5fb0e7378e2737b88cf
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-27 23:13:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 297.47 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,16 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> map = new HashMap<>();
+        Arrays.sort(participant);
+        Arrays.sort(completion);
 
-        for (String p : participant) {
-            map.put(p, map.getOrDefault(p, 0) + 1);
-        }
-
-        for (String c : completion) {
-            map.put(c, map.get(c) - 1);
-        }
-
-        for (String key : map.keySet()) {
-            if (map.get(key) > 0) {
-                return key;
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
             }
         }
 
-        return "";
+        return participant[participant.length - 1];
     }
 }
