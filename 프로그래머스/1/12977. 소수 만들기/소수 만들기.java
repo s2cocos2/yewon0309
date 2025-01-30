ec0da442cd4aa192b4f9815095e726d09cdceac9
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-30 23:48:25 +0900

    [level 1] Title: 소수 만들기, Time: 1.85 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,38 +1,26 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
     public int solution(int[] nums) {
-        List<Integer> combinations = new ArrayList<>();
-        combine(nums, 0, 3, 0, combinations);
-        
         int count = 0;
-        for (int sum : combinations) {
-            if (isPrime(sum)) {
-                count++;
+        int len = nums.length;
+
+        for (int i = 0; i < len - 2; i++) {
+            for (int j = i + 1; j < len - 1; j++) {
+                for (int k = j + 1; k < len; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        count++;
+                    }
+                }
             }
         }
+
         return count;
     }
 
-    private void combine(int[] nums, int start, int r, int sum, List<Integer> combinations) {
-        if (r == 0) {
-            combinations.add(sum);
-            return;
-        }
-        for (int i = start; i < nums.length; i++) {
-            combine(nums, i + 1, r - 1, sum + nums[i], combinations);
-        }
-    }
-
     private boolean isPrime(int num) {
-        if (num < 2) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
-            }
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
         }
         return true;
     }
