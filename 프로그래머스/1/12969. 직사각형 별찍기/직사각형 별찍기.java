738318c7e14e5b5a40124afb2682a4466a0c69be
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-01-31 23:37:29 +0900

    [level 1] Title: 직사각형 별찍기, Time: 248.41 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.close();
+
+        StringBuilder sb = new StringBuilder();
+        
+        String row = "*".repeat(n);
+
+        for (int i = 0; i < m; i++) {
+            sb.append(row).append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
