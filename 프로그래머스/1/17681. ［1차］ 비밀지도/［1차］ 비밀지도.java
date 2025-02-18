2ddb14eb2fe0ba0f78f91a6deb5bfa34ff5e5843
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-02-18 23:58:02 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.54 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,22 +1,17 @@
 public class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] result = new String[n];
-
+        
         for (int i = 0; i < n; i++) {
-            int combined = arr1[i] | arr2[i];
-            StringBuilder row = new StringBuilder();
-
-            for (int j = 0; j < n; j++) {
-                if ((combined & (1 << (n - 1 - j))) != 0) {
-                    row.append("#");
-                } else {
-                    row.append(" ");
-                }
-            }
-
-            result[i] = row.toString();
+            String binary = Integer.toBinaryString(arr1[i] | arr2[i]);
+            
+            binary = String.format("%" + n + "s", binary);
+            
+            binary = binary.replace('1', '#').replace('0', ' ');
+            
+            result[i] = binary;
         }
-
+        
         return result;
     }
 }
