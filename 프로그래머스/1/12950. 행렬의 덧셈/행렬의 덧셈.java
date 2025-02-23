fb5c2adf3cc78f595336d96e492c79360f81b93a
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-02-23 23:17:14 +0900

    [level 1] Title: 행렬의 덧셈, Time: 11.31 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,15 +1,11 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int rows = arr1.length;
-        int cols = arr1[0].length;
-        int[][] answer = new int[rows][cols];
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                answer[i][j] = arr1[i][j] + arr2[i][j];
-            }
-        }
-
-        return answer;
+        return IntStream.range(0, arr1.length)
+                .mapToObj(i -> IntStream.range(0, arr1[i].length)
+                        .map(j -> arr1[i][j] + arr2[i][j])
+                        .toArray())
+                .toArray(int[][]::new);
     }
 }
