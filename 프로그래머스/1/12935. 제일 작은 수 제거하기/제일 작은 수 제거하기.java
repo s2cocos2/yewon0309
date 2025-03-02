af5a0618a8b0b48068847e5d066b2d1b4eec6f2d
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-03-02 23:45:04 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 14.92 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,21 +1,21 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
         if (arr.length == 1) return new int[]{-1};
 
-        int min = arr[0], minIndex = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-                minIndex = i;
+        int min = Integer.MAX_VALUE;
+        for (int num : arr) {
+            min = Math.min(min, num);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int num : arr) {
+            if (num != min) {
+                list.add(num);
             }
         }
 
-        int[] result = new int[arr.length - 1];
-        System.arraycopy(arr, 0, result, 0, minIndex);
-        System.arraycopy(arr, minIndex + 1, result, minIndex, arr.length - minIndex - 1);
-
-        return result;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
