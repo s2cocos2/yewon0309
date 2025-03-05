6ff8ef94d0e7515e796f178282e2b4cf28dab702
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-03-05 23:59:34 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,17 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        for(int i=1;i<=n;i++){
-            if(n % i == 0){
-                list.add(i);
-            }
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) sum += i;
         }
-
-        answer = list.stream().mapToInt(Integer::intValue).sum();
-        return answer;
+        return sum + n;
     }
 }
