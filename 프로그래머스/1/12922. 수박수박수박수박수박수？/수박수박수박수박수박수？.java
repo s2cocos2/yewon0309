5ba8d9b605f1d99d3c8557c63d3188d50a8352a4
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-03-08 02:14:52 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.56 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,15 +1,5 @@
 class Solution {
     public String solution(int n) {
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                sb.append("수");
-            } else {
-                sb.append("박");
-            }
-        }
-        
-        return sb.toString();
+        return "수박".repeat(n / 2) + (n % 2 == 1 ? "수" : "");
     }
 }
