0128bba53fa9a104eea7c6204da7e0c9155f4c52
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-03-09 23:57:19 +0900

    [level 1] Title: 소수 찾기, Time: 13.97 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,30 +1,19 @@
 class Solution {
     public int solution(int n) {
-        // n 이하의 숫자 중 소수를 찾기 위한 배열
+        if (n < 2) return 0;
+
         boolean[] isPrime = new boolean[n + 1];
-        
-        // 2부터 n까지 모든 수를 소수로 가정
+        int count = 0;
+
         for (int i = 2; i <= n; i++) {
-            isPrime[i] = true;
-        }
-        
-        // 에라토스테네스의 체 알고리즘
-        for (int i = 2; i * i <= n; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    isPrime[j] = false;
+            if (!isPrime[i]) {
+                count++;
+                for (int j = i; j <= n; j += i) {
+                    isPrime[j] = true;
                 }
             }
         }
-        
-        // 소수의 개수를 세기
-        int count = 0;
-        for (int i = 2; i <= n; i++) {
-            if (isPrime[i]) {
-                count++;
-            }
-        }
-        
+
         return count;
     }
 }
