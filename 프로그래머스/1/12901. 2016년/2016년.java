30429af580aacd2ef3039ff69e86aa14d33daf98
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-03-20 23:20:53 +0900

    [level 1] Title: 2016년, Time: 12.65 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,21 +1,7 @@
-import java.time.LocalDate;
-import java.time.DayOfWeek;
+import java.time.*;
 
-public class Solution {
+class Solution {
     public String solution(int a, int b) {
-        LocalDate date = LocalDate.of(2016, a, b);
-        
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        
-        switch (dayOfWeek) {
-            case MONDAY: return "MON";
-            case TUESDAY: return "TUE";
-            case WEDNESDAY: return "WED";
-            case THURSDAY: return "THU";
-            case FRIDAY: return "FRI";
-            case SATURDAY: return "SAT";
-            case SUNDAY: return "SUN";
-            default: return "";
-        }
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
     }
 }
