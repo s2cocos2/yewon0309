a7fff4249eee340fa1f77763e4d6b953f2665824
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-03-22 22:58:58 +0900

    [level 1] Title: 2016년, Time: 12.65 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,13 +1,7 @@
+import java.time.*;
+
 class Solution {
     public String solution(int a, int b) {
-        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-        int[] monthDays = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        
-        int totalDays = b - 1;
-        for (int i = 1; i < a; i++) {
-            totalDays += monthDays[i];
-        }
-        
-        return days[totalDays % 7];
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
     }
 }
