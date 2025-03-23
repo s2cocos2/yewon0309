288884339a0b98df1ebdb30b984f3fa8886443b9
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-03-23 23:03:21 +0900

    [level 1] Title: 폰켓몬, Time: 20.60 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,8 @@
+import java.util.stream.Collectors;
 import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        Set<Integer> set = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            set.add(nums[i]);
-        }
-
-        answer = Math.min(nums.length / 2, set.size());
-        return answer;
+        return Math.min(nums.length / 2, (int) Arrays.stream(nums).boxed().collect(Collectors.toSet()).size());
     }
 }
