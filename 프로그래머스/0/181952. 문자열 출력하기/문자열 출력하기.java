06372ed1fdc92acde6a344d8d20320c4d95b9826
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-03-24 23:04:30 +0900

    [level 0] Title: 문자열 출력하기, Time: 255.83 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        System.out.print(a);
+        String str = sc.next();
+        System.out.print(str);
     }
 }
