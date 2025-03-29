c4b602552229f9af19c506886c0453215861f43b
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-03-29 23:28:53 +0900

    [level 0] Title: 특수문자 출력하기, Time: 138.75 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -1,5 +1,7 @@
-public class Solution  {
+import java.util.Scanner;
+
+public class Solution {
     public static void main(String[] args) {
-        System.out.println("!@#$%^&*(\\'\"<>?:;");
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
     }
 }
