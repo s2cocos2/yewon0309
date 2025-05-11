a2dac50b5a976d825f0cae7775de1b30faac4463
Yewon Lee <120706046+s2cocos2@users.noreply.github.com>
2025-05-11 23:21:35 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,4 +1,6 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME from ANIMAL_INS
-where lower(NAME) like '%el%' and ANIMAL_TYPE = 'Dog'
-order by NAME
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = 'Dog'
+AND LOWER(NAME) LIKE '%el%'
+ORDER BY NAME;
